4fc4e9374c92f48aa607877feb3939d505b212c1
Rawan Mohammed <rawanmohaamed213@gmail.com>
2025-08-28 05:38:42 +0300

    Update BasePage.java

--- src/main/java/com/lambdatest/pages/BasePage.java
+++ src/main/java/com/lambdatest/pages/BasePage.java
@@ -1,6 +1,43 @@
 package com.lambdatest.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import com.google.common.base.Function;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class BasePage {
 
-    
+    public static WebDriver driver;
+    WebDriverWait wait;
+
+    public static void setDriver(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+
+    public static void setWait(WebDriverWait wait) {
+        BasePage.wait = wait;
+    }
+
+    public WebElement find(By locator){
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public void click(By locator){
+        find(locator).click();
+    }
+
+    public String getText(By locator){
+        String text = find(locator).getText();
+        System.out.println("Text: " + text);
+        return text;
+    }
 }
